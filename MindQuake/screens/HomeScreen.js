104f07758c2262a07ba036659fe16e151e7c965b
Andreu Verdés <averdes12@gmail.com>
2025-03-24 13:52:01 +0100

    home screen done

--- MindQuake/screens/HomeScreen.js
+++ MindQuake/screens/HomeScreen.js
@@ -1,16 +1,62 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { Image } from '@rneui/themed';
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Bienvenido a la Home Screen</Text>
-      <Button
-        title="Ir a detalles"
-        onPress={() => navigation.navigate('Details')}
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/logo.png')}
+        style={styles.image}
       />
+      <TouchableOpacity
+        style={[styles.button, styles.buttonYellow]}
+        onPress={() => navigation.navigate('Details')}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, styles.buttonBlue]}
+        onPress={() => navigation.navigate('AnotherScreen')}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    marginBottom: 50,
+  },
+
+  button: {
+    width: '50%',
+    padding: 15,
+    borderRadius: 50,  // Border radius para los botones
+    marginBottom: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonYellow: {
+    borderColor: 'gold',        // Borde dorado
+    borderWidth: 8,                               
+  },
+  buttonBlue: {
+    borderColor: 'dodgerblue', // Borde azul
+    borderWidth: 8,            // Ancho del borde
+  },
+  buttonText: {
+    color: 'black',             // Color del texto
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+});
 
 export default HomeScreen;
